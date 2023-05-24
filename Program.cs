e579ac601315e1e36c03f5fe749a0b691b82effe
Arsalaann <arsalankhanxk@gmail.com>
2023-05-24 12:15:47 +0530

    validate lastname1

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
 ﻿namespace Day19Problems{
     class Program{
         public static void Main(string [] args){
-            Console.Write("Enter a name: ");
+            Console.Write("Enter First and last name: ");
             string name=Console.ReadLine();
             while(Validate.ValidateName(name)!=true){
-                Console.Write("Enter a valid name: ");
+                Console.Write("Enter a valid Firstname: ");
                 name=Console.ReadLine();
+            }
+            string lastName=Console.ReadLine();
+            while(Validate.ValidateName(lastName)!=true){
+                Console.Write("Enter a valid Lastname: ");
+                lastName=Console.ReadLine();
             }
             
         }
